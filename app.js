ac8441e82c9587b2d4428767368d667217b27c80
Mayankraj0410 <93092945+Mayankraj0410@users.noreply.github.com>
2024-05-20 17:59:35 +0530

    Update app.js with .env variable

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js"); 
-//const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+
+const MONGO_URL= process.env.MONGO_URL || "mongodb://127.0.0.1:27017/wanderlust";
 const path = require("path");
 const methodOverride = require("method-override"); 
 const ejsMate = require("ejs-mate");
